e2cd0a8a83d09456a6d430b9d6613638d9563777
Aakash <aakash2506c@gmail.com>
2024-01-23 09:29:22 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-// require('dotenv').config({path:'./env'});
-import mongoose from "mongoose";
-import express from "express";
+
 import dotenv from "dotenv";
 import connectDB from './db/index.js'
+import app from "./app.js";
 
 
 // Load the configuration
 dotenv.config({ path: `../.env`});
-const app=express();
-const port=process.env.PORT;
-connectDB();
+const port=process.env.PORT||3000;
+connectDB().then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server running at ${port}`)
+    })
+}).catch((err)=>{
+  console.log(err);
+})
 
 
