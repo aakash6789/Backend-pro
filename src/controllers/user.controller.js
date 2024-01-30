4e8abc451a47d767fc68fe526d50241d2924deec
Aakash <aakash2506c@gmail.com>
2024-01-30 17:42:39 +0530

    Logout functionality designed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -85,8 +85,15 @@ const loginUser=asyncHandler(async(req,res)=>{
 });
 
 const logOutUser=asyncHandler(async(req,res)=>{
-    
+  await User.findByIdAndUpdate(req.user._id,{
+    $set:{refreshToken:undefined}
+  })
+  const options={
+    httpOnly:true,
+    secure:true
+   }
+   return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(new ApiResponse(200,{},"Logged out successfully"));
 });
 
 
-export {registerUser};
+export {registerUser,logOutUser,loginUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from '../middlewares/multer.middleware.js'
+import { registerUser,logOutUser,loginUser } from "../controllers/user.controller.js";
+import {upload} from '../middlewares/multer.middleware.js';
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=Router();
 router.post('/register',upload.fields([{
     name:'avatar',
@@ -9,5 +10,7 @@ router.post('/register',upload.fields([{
     name:'coverImage',
     maxCount:1
 }]),registerUser);
+router.post('/login',loginUser);
+router.post('/logout',verifyJWT,logOutUser);
 
 export default router;
